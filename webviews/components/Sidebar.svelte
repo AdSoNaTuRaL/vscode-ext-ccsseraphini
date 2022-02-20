22469fa7c21f94d1118f0746de17ee56a9b94dbf
adsonatural <adsonhenriquesilva@gmail.com>
2022-02-20 14:24:24 -0300

    refactor: format code, add validations, styles and more

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,37 +1,54 @@
 <script lang="ts">
-  const suffix = '\ncc @sseraphini';
+  import { suffix, maxTweetLength } from "../../src/constants";
   let text = "";
 </script>
 
 <div style="display: flex;">
-  <img 
-    src="https://pbs.twimg.com/profile_images/1494329046678659072/RprvW5s4_400x400.jpg" 
-    alt="Seraphini Icon" 
+  <img
+    src="https://pbs.twimg.com/profile_images/1494329046678659072/RprvW5s4_400x400.jpg"
+    alt="Seraphini Icon"
     style="max-width: 100px; border-radius: 100px; padding: 8px; margin: auto;"
   />
 </div>
 
-<form on:submit|preventDefault={() => {
+<form
+  on:submit|preventDefault={() => {
     text = "";
-    console.log('send to twitter API');
-}}>
-  <textarea 
-    bind:value={text} 
-    id="tweet" 
-    rows="10" 
-    placeholder="Write your tweet concept/question here" 
-    on:input={() => {
-      // TODO: Add Validation to Character limit 264
-    }} 
+  }}
+>
+  <textarea
+    bind:value={text}
+    id="tweet"
+    rows="10"
+    placeholder="Write your tweet concept/question here"
   />
 
-  <p>Typed characters: {text.length}</p>
-  <p>Maximum characters: 264</p>
-  
-  
-  <button style="margin-top: 16px;" on:click={() => {
-    // Add Validation before send to twitter a
-    const tweet = encodeURIComponent(`${text}${suffix}`);
-    tsvscode.postMessage({ type: 'onInfo', value: tweet });
-  }}>Tweet</button>
+  <p class:exceeded={text.length + suffix.length > maxTweetLength}>
+    Typed characters: {text.length}
+  </p>
+  <p>Maximum characters: {maxTweetLength}</p>
+
+  <!-- svelte-ignore missing-declaration -->
+  <button
+    style="margin-top: 16px;"
+    on:click={() => {
+      const tweet = encodeURIComponent(`${text}${suffix}`);
+
+      if (tweet.length > maxTweetLength) {
+        tsvscode.postMessage({
+          type: "onError",
+          value: "Your tweet exceeded the maximum of characters allowed!",
+        });
+        return;
+      }
+
+      tsvscode.postMessage({ type: "onInfo", value: tweet });
+    }}>Tweet</button
+  >
 </form>
+
+<style>
+  .exceeded {
+    color: red;
+  }
+</style>
