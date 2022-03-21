76e9d419c48687d1cd9f12685e21938ac2e41de9
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-21 12:24:34 -0300

    refactor: change url img and order to calc character

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -9,7 +9,7 @@
 
 <div style="display: flex;">
   <img
-    src="https://pbs.twimg.com/profile_images/1494329046678659072/RprvW5s4_400x400.jpg"
+    src="https://unavatar.io/twitter/sseraphini"
     alt="Seraphini Icon"
     style="max-width: 100px; border-radius: 100px; padding: 8px; margin: auto;"
   />
@@ -27,7 +27,7 @@
     placeholder="Write your tweet concept/question here"
   />
 
-  <p class:exceeded={text.length + suffix.length > maxTweetLength}>
+  <p class:exceeded={(text.length + suffix.length ) > maxTweetLength}>
     Typed characters: {text.length}
   </p>
   <p>Maximum characters: {maxTweetLength}</p>
