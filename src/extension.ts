7379188ba18834dee4d8ab059469242b902c79a4
adsonatural <adsonhenriquesilva@gmail.com>
2022-03-21 12:24:00 -0300

    comment dev command

--- src/extension.ts
+++ src/extension.ts
@@ -19,14 +19,28 @@ export function activate(context: vscode.ExtensionContext) {
     })
   );
 
-  context.subscriptions.push(
-    vscode.commands.registerCommand("ccsseraphini.refresh", async () => {
-      await vscode.commands.executeCommand("workbench.action.closeSidebar");
-      await vscode.commands.executeCommand(
-        "workbench.view.extension.ccsseraphini-view"
-      );
-    })
-  );
+  /**
+   * Uncomment the code above, if u want a fast refresh while coding. Just for dev enviroment
+   * Also edit package.json add new command to this
+   * @example 
+   *  "onCommand:ccsseraphini.refresh",
+   * 
+   * and
+   *
+   *  {
+        "command": "ccsseraphini.refresh",
+        "category": "cc @sseraphini",
+        "title": "Refresh"
+      }
+   * */ 
+  // context.subscriptions.push(
+  //   vscode.commands.registerCommand("ccsseraphini.refresh", async () => {
+  //     await vscode.commands.executeCommand("workbench.action.closeSidebar");
+  //     await vscode.commands.executeCommand(
+  //       "workbench.view.extension.ccsseraphini-view"
+  //     );
+  //   })
+  // );
 }
 
 // this method is called when your extension is deactivated
